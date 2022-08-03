db8bda41ade190d4b7cf4ce92b0dcbe25b918550
dani09barreto <danielf.barreto@javeriana.edu.co>
2022-08-03 18:48:00 -0500

    termina funcionalidad de consola

--- src/Controller.cxx
+++ src/Controller.cxx
@@ -2,15 +2,13 @@
 #include <iostream>
 
 void Controller::Cargar (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::conteo(Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);    
 }
 void Controller::salir(Shell::argv_t argvs, Shell command){
@@ -21,56 +19,52 @@ void Controller::salir(Shell::argv_t argvs, Shell command){
     exit(3);
 }
 void Controller::listar_secuencias (Shell::argv_t argvs, Shell command) {
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 } 
 void Controller::histograma (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::es_subsecuencia (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::enmascarar (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::guardar (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::codificar (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::decodificar (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::ruta_mas_corta (Shell::argv_t argvs, Shell command){
-    if (argvs[1].compare("help") == 0)
-        std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
         throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
 void Controller::base_remota (Shell::argv_t argvs, Shell command){
+    Controller::help(argvs, command);
+    if (argvs.size() != command.getArgc())
+        throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
+}
+
+void Controller::help(Shell::argv_t argvs, Shell command){
     if (argvs[1].compare("help") == 0)
         std::cout << command.getCommandUsage() <<" "<< command.getCommandDescription() << "\n";
-    else if (argvs.size() != command.getArgc())
-        throw Shell::SyntaxError(Shell::SyntaxError::TypeError::ERROR_AGV);
 }
